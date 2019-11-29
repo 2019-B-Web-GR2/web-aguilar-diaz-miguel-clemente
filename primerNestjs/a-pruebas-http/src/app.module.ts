70d0012821d9f2ec5f1b47b3bf6441523d1eb652
EkimXD <miguelclementeaguilardiaz@hotmail.com>
2019-11-29 16:02:25 -0500

    docker y base de datos

--- primerNestjs/a-pruebas-http/src/app.module.ts
+++ primerNestjs/a-pruebas-http/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario/usuario.entity";
 
 @Module({
-  imports: [],
+  imports: [
+       TypeOrmModule.forRoot({
+         type: 'mysql',
+         host:'localhost',
+         port: 3306,
+         username: 'root',
+         password: '1234',
+         database: 'web',
+         entities:[
+             UsuarioEntity
+         ],
+         synchronize:true
+       })
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
